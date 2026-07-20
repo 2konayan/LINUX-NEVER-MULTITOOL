974ea095760e347a34894d57591002889c190b7d
j4mzqkq844-beep <ayan.cristian.clivet@icloud.com>
2026-07-20 17:02:20 -0400

    Update NEVER.py with improved code

--- NEVER.py
+++ NEVER.py
@@ -7,10 +7,10 @@ option = input("""1.NMAP PORT SCAN
          2.PINGER
          3. Close NEVER """)
 if option == "1":
-    target_ip = input("What is the target's ip")
+    target_ip = input("What is the target's ip ")
     os.system("nmap " + target_ip)
 elif option == "2":
-    target_ip = input("What is the ip of the system you Wanna ping?")
+    target_ip = input("What is the ip of the system you Wanna ping? ")
     os.system("ping " + target_ip)
 elif option == "3":
     exit()

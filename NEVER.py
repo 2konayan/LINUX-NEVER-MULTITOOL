4fef4b5904bdf865202dd627066593152e504f4d
2konayan <ayan.cristian.clivet@icloud.com>
2026-07-22 15:35:53 -0400

    NEVER.py quote of the day update

--- NEVER.py
+++ NEVER.py
@@ -1,9 +1,9 @@
 import pyfiglet
 import os
+import random
 from colorama import init, Fore, Style
 
 init(autoreset=True)
-
 os.system("aplay ~/Downloads/boing_x.wav 2>/dev/null")
 
 text = pyfiglet.figlet_format("NEVER MULTITOOL")
@@ -12,13 +12,29 @@ print(Style.RESET_ALL)
 
 print("EDUCATIONAL PURPOSES ONLYY!!!")
 print("SYSADMIN TOOL")
+
+quotes = [
+    "Hacking is not a crime, it's a skill.",
+    "The quieter you become, the more you can hear.",
+    "Knowledge is free. We are anonymous. We are Legion.",
+    "Expect the unexpected.",
+    "There is no patch for human stupidity.",
+    "Hackers rule the world, be a part of them.",
+    "Keep calm and hack ethically."
+]
+
+print(Fore.YELLOW + random.choice(quotes))
+print(Style.RESET_ALL)
+
 option = input("""1.NMAP PORT SCAN
          2.PINGER
          3.SYN DOS
          4.MAC SPOOFING
          5.WHOIS INFO
          6.NSLOOKUP
-         7.EXIT NEVER""")
+         7.EXIT NEVER
+         """)
+
 if option == "1":
     target_ip = input("What is the target's ip ")
     os.system("sudo nmap " + target_ip)
